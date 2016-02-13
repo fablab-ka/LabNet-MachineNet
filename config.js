bbb99ea1a15df69bfb3a8df015639912cba45145
Sven Hecht <sven.hecht@citrix.com>
2016-02-13 21:16:44 +0100

    added env var for machinenet and usernet configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   //dbUrl: 'mongodb://192.168.99.101:27017/machinenet',
-  dbUrl: 'mongodb://mongo/machinenet',
-  userNetApi: 'http://usernet/api',
+  dbUrl: process.env.machineNetUrl || 'mongodb://mongo/machinenet',
+  userNetApi: process.env.usernetUrl || 'http://usernet/api',
   discoveryPort: 1212,
   discoveryInterval: 3000,
   machinePingInterval: 3000
